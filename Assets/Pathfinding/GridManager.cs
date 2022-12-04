733730b5e39e743e4347bbd981bb669ee002e826
Sartosh-B <85825443+Sartosh-B@users.noreply.github.com>
2022-12-04 10:57:01 +0100

    Pathfinding_1

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node node;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2Int gridsize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+    public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
+    private void Awake()
     {
-        Debug.Log(node.coordinates);
-        Debug.Log(node.isWalkable);
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Node GetNode(Vector2Int coordinates)
     {
-        
+        if (grid.ContainsKey(coordinates))
+        {
+            return grid[coordinates];
+        }
+        return null;
+    }
+
+
+    private void CreateGrid()
+    {
+        for (int x = 0; x < gridsize.x; x++)
+        {
+            for (int y = 0; y < gridsize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                grid.Add(coordinates, new Node(coordinates, true));               
+            }
+        }
     }
 }
